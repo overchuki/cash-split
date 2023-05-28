28d088bbaa8801c22561d978d4f7bb6952c46cfb
Igor Overchuk <igor.t.over@gmail.com>
2023-05-28 12:01:57 -0600

    close menu on global click

--- src/app/components/userMenu.tsx
+++ src/app/components/userMenu.tsx
@@ -1,30 +1,46 @@
 import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
 
 interface props {
-    isOpen: boolean;
+    popupClassName: string;
+    menuIsOpen: boolean;
+    setMenuIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function UserMenu({ isOpen }: props) {
+export default function UserMenu({ popupClassName, menuIsOpen, setMenuIsOpen }: props) {
+    window.addEventListener("click", (e) => {
+        const target = e.target as HTMLDivElement | HTMLLinkElement;
+        if (!target.classList.contains(popupClassName) && menuIsOpen) {
+            setMenuIsOpen(false);
+        }
+    });
+
     return (
         <div className="relative">
             <div
                 className={
-                    isOpen ? "absolute top-1 -right-5 bg-gray-900 py-3 border-2 border-sky-950 rounded-xl" : "hidden"
+                    menuIsOpen
+                        ? `absolute top-1 -right-5 bg-gray-900 py-3 border-2 border-sky-950 rounded-xl w-32 ${popupClassName}`
+                        : `hidden ${popupClassName}`
                 }
             >
-                <div className="flex flex-col">
-                    <Link href="#" className="flex py-1 px-6 justify-center hover:cursor-pointer hover:bg-gray-800">
+                <div className={`flex flex-col z-10 ${popupClassName}`}>
+                    <Link
+                        href="#"
+                        className={`flex py-1 px-6 justify-start text-md font-light hover:cursor-pointer hover:bg-gray-800 ${popupClassName}`}
+                    >
                         Account
                     </Link>
                     <Link
                         href="#"
-                        className="flex py-1 mb-0.5 px-6 justify-center hover:cursor-pointer hover:bg-gray-800"
+                        className={`flex py-1 px-6 justify-start text-md font-light hover:cursor-pointer hover:bg-gray-800 ${popupClassName}`}
                     >
                         Settings
                     </Link>
+                    <div className={`flex h-0 my-1 border-t border-opacity-50 border-gray-500 ${popupClassName}`}></div>
                     <Link
                         href="#"
-                        className="flex py-1 mt-0.5 px-6 justify-center border-t border-opacity-50 border-gray-500 hover:cursor-pointer hover:bg-gray-800"
+                        className={`flex py-1 px-6 justify-start text-md font-light hover:cursor-pointer hover:bg-gray-800 ${popupClassName}`}
                     >
                         Logout
                     </Link>
