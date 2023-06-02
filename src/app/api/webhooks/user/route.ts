e8a00d8bd971f10f8e4b01fde7f5f846451015a1
Igor Overchuk <igor.t.over@gmail.com>
2023-06-01 23:14:37 -0600

    api route response

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -1,44 +1,46 @@
 import type { WebhookEvent, UserJSON } from "@clerk/clerk-sdk-node";
 import { Webhook, WebhookRequiredHeaders } from "svix";
 import { getErrorResponse } from "@/app/utils/general";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
     const body = (await request.json()) as WebhookEvent;
     console.log(body);
 
     const payload = JSON.stringify(body);
-    const headers: WebhookRequiredHeaders = {
+    console.log("HEADERS: ", request.headers);
+    const svixHeaders: WebhookRequiredHeaders = {
         "svix-id": request.headers.get("svix-id") ?? "invalid_id",
         "svix-timestamp": request.headers.get("svix-timestamp") ?? "invalid_timestamp",
         "svix-signature": request.headers.get("svix-signature") ?? "invalid_signature",
     };
     console.log("payload: " + payload);
-    console.log("headers: " + JSON.stringify(headers));
+    console.log("headers: " + JSON.stringify(svixHeaders));
 
     const secret = process.env.SVIX_SECRET;
     if (secret === undefined) {
         return getErrorResponse(500, "Server error");
     }
     const wh = new Webhook(secret);
-    let msg;
     try {
-        msg = wh.verify(payload, headers);
+        wh.verify(payload, svixHeaders);
     } catch (err) {
         return getErrorResponse(403, "Request is invalid");
     }
-    console.log("msg: " + JSON.stringify(msg));
 
     switch (body.type) {
         case "user.created":
-            console.log(body.data);
+            console.log("USER CREATED EVENT");
             break;
         case "user.updated":
-            console.log(body.data);
+            console.log("USER UPDATED EVENT");
             break;
         case "user.deleted":
-            console.log(body.data);
+            console.log("USER DELETED EVENT");
             break;
     }
+
+    return NextResponse.json({ message: "Webhook received" }, { status: 200 });
 }
 
 const createUser = (user: UserJSON): void => {};
