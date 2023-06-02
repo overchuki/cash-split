d6b18c5c0fc1f7ce13bd9a411abe6377fa4b9c17
Igor Overchuk <igor.t.over@gmail.com>
2023-06-01 23:30:24 -0600

    webhook all handlers

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -1,21 +1,18 @@
-import type { WebhookEvent, UserJSON } from "@clerk/clerk-sdk-node";
+import type { WebhookEvent, UserJSON, DeletedObjectJSON } from "@clerk/clerk-sdk-node";
 import { Webhook, WebhookRequiredHeaders } from "svix";
 import { getErrorResponse } from "@/app/utils/general";
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
     const body = (await request.json()) as WebhookEvent;
-    console.log(body);
 
     const payload = JSON.stringify(body);
-    console.log("HEADERS: ", request.headers);
     const svixHeaders: WebhookRequiredHeaders = {
         "svix-id": request.headers.get("svix-id") ?? "invalid_id",
         "svix-timestamp": request.headers.get("svix-timestamp") ?? "invalid_timestamp",
         "svix-signature": request.headers.get("svix-signature") ?? "invalid_signature",
     };
-    console.log("payload: " + payload);
-    console.log("headers: " + JSON.stringify(svixHeaders));
 
     const secret = process.env.SVIX_SECRET;
     if (secret === undefined) {
@@ -28,23 +25,62 @@ export async function POST(request: NextRequest) {
         return getErrorResponse(403, "Request is invalid");
     }
 
-    switch (body.type) {
-        case "user.created":
-            console.log("USER CREATED EVENT");
-            break;
-        case "user.updated":
-            console.log("USER UPDATED EVENT");
-            break;
-        case "user.deleted":
-            console.log("USER DELETED EVENT");
-            break;
+    try {
+        switch (body.type) {
+            case "user.created":
+                await createUser(body.data);
+                break;
+            case "user.updated":
+                await updateUser(body.data);
+                break;
+            case "user.deleted":
+                await deleteUser(body.data);
+                break;
+        }
+    } catch (err) {
+        return NextResponse.json({ message: "Webhook received, error processing" }, { status: 500 });
     }
 
     return NextResponse.json({ message: "Webhook received" }, { status: 200 });
 }
 
-const createUser = (user: UserJSON): void => {};
+const createUser = async (user: UserJSON) => {
+    const prisma = new PrismaClient();
+    const res = await prisma.user.create({
+        data: {
+            clerk_id: user.id,
+            username: user.username,
+            email: user.email_addresses.find((ema) => ema.id === user.primary_email_address_id)?.email_address,
+            name: user.first_name + " " + user.last_name,
+        },
+    });
+    console.log("USER CREATED: ", JSON.stringify(res));
+};
 
-const updateUser = (user: UserJSON): void => {};
+const updateUser = async (user: UserJSON) => {
+    const prisma = new PrismaClient();
+    const res = await prisma.user.update({
+        where: {
+            clerk_id: user.id,
+        },
+        data: {
+            clerk_id: user.id,
+            username: user.username,
+            email: user.email_addresses.find((ema) => ema.id === user.primary_email_address_id)?.email_address,
+            name: user.first_name + " " + user.last_name,
+        },
+    });
+    console.log("USER UPDATED: ", JSON.stringify(res));
+};
 
-const deleteUser = (user: UserJSON): void => {};
+const deleteUser = async (user: DeletedObjectJSON) => {
+    if (user.deleted) {
+        const prisma = new PrismaClient();
+        const res = await prisma.user.delete({
+            where: {
+                clerk_id: user.id,
+            },
+        });
+        console.log("USER DELETED: ", JSON.stringify(res));
+    }
+};
